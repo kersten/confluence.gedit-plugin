1f3f17f23d435e076f0efe298116476dc0f21d4c
Kersten Burkhardt <kerstenk@gmail.com>
2010-12-23 11:01:52 +0100

    Added some Messageboxes and a test credentials button

--- confluence/__init__.py
+++ confluence/__init__.py
@@ -1,9 +1,17 @@
 import gedit
+import gtk
 import options
-import confluencerpclib
+import os
+import sys
+
+from confluencerpclib import Confluence
+from confluencewidget import ConfluenceBrowser
 
 
 class confluencePluginPlugin(gedit.Plugin):
+
+    def __init__(self):
+        gedit.Plugin.__init__(self)
 
     def create_configure_dialog(self):
         return self.options.create_configure_dialog()
@@ -17,15 +25,45 @@ class confluencePluginPlugin(gedit.Plugin):
         #print confluenceApi.singleton().getSpaces()
         #print confluenceApi.singleton().getPages('SupportDaphne')
         #print confluenceApi.singleton().getPageById('29458504')
-        self.confluence = confluencerpclib.connect(self.options.url)
-        self.confluence.login(self.options.username, self.options.password)
+        #self.confluence = Confluence(self.options.url, True)
+        #self.confluence.login(self.options.username, self.options.password)
 
-        print self.confluence.token
+        #print self.confluence.token
+
+        #self.confluence.getPage('29458504')
+
+        if self.options.loginPassed is True:
+            self.loadConfluenceBrowser(window)
         pass
 
     def deactivate(self, window):
         self.confluence.logout()
+        if self.options.loginPassed is True:
+            self.unloadConfluenceBrowser(window)
         pass
 
     def update_ui(self, window):
         pass
+
+    def loadConfluenceBrowser(self, window):
+        panel = window.get_side_panel()
+        image = gtk.Image()
+
+        filename = os.path.join(sys.path[0], "confluence", "pixmaps",
+                                "confluence.png")
+        pixbuf = gtk.gdk.pixbuf_new_from_file(filename)
+
+        image.set_from_pixbuf(pixbuf)
+
+        self.confluencebrowser = ConfluenceBrowser(window)
+        panel.add_item(self.confluencebrowser, "Confluence Browser", image)
+
+        # store per window data in the window object
+        windowdata = {"ConfluenceBrowser": self.confluencebrowser}
+
+    def unloadConfluenceBrowser(self, window):
+        pane = window.get_side_panel()
+        pane.remove_item(self.confluencebrowser)
+        windowdata = window.get_data("ConfluenceBrowserPluginWindowDataKey")
+        manager = window.get_ui_manager()
+        manager.remove_action_group(windowdata["action_group"])
--- confluence/options.py
+++ confluence/options.py
@@ -2,7 +2,7 @@ import gtk.glade
 import gconf
 import os
 import sys
-import confluenceApi
+from confluencerpclib import Confluence
 
 
 class options():
@@ -21,6 +21,7 @@ class options():
         self.username = ""
         self.password = ""
         self.url = "https://your.confluence.url/rpc"
+        self.loginPassed = False
 
         # get the gconf keys, or stay with default if key not set
         try:
@@ -35,19 +36,22 @@ class options():
             self.url = self.client.get_string(
                 self.__gconfDir + "/url") or self.url
 
+            self.loginPassed = self.client.get_bool(
+                self.__gconfDir + "/loginPassed") or self.loginPassed
+
         except Exception, e: # catch, just in case
             print e
 
     def create_configure_dialog(self):
         self.dialogGladeFile = os.path.join(sys.path[0], "confluence",
-            "confluencePluginConfigureDialog.glade")
+                               "confluencePluginConfigureDialog.glade")
 
         self.dialog = gtk.Builder()
         self.dialog.add_from_file(self.dialogGladeFile)
         self.dialog.connect_signals(self)
 
         self.dialogWindow = self.dialog.get_object(
-            "confluencePluginConfigureDialog")
+                            "confluencePluginConfigureDialog")
 
         self.dialogWindowUsername = self.dialog.get_object("loginUsername")
         self.dialogWindowUsername.set_text(self.username)
@@ -59,7 +63,8 @@ class options():
         self.dialogWindowUrl.set_text(self.url)
 
         self.dialogSignals = {
-            "on_closeButton_clicked": self.on_closeButton_clicked}
+            "on_closeButton_clicked": self.on_closeButton_clicked,
+            "on_testButton_clicked": self.on_testButton_clicked}
         self.dialog.connect_signals(self.dialogSignals)
 
         return self.dialogWindow
@@ -73,24 +78,50 @@ class options():
         self.client = gconf.client_get_default()
 
         self.client.set_string(self.__gconfDir + "/username",
-            self.username)
+                               self.username)
         self.client.set_string(self.__gconfDir + "/password",
-            self.password)
+                               self.password)
         self.client.set_string(self.__gconfDir + "/url",
-            self.url)
+                               self.url)
+
+        if self.loginPassed is False:
+            message = gtk.MessageDialog(None,
+                gtk.DIALOG_MODAL | gtk.DIALOG_DESTROY_WITH_PARENT,
+                gtk.MESSAGE_ERROR, gtk.BUTTONS_OK, "You have not verified "
+                                                   "your Confluence "
+                                                   "credentials, please "
+                                                   "click \"test\" to "
+                                                  "do this!")
+            message.run()
+            message.destroy()
+        else:
+            self.dialogWindow.destroy()
+
+    def on_testButton_clicked(self, widget):
+        self.username = self.dialogWindowUsername.get_text()
+        self.password = self.dialogWindowPassword.get_text()
+        self.url = self.dialogWindowUrl.get_text()
 
         if self.confluenceLogin() is True:
-            self.dialogWindow.destroy()
+            msg = "Login successful"
+            
+            self.loginPassed = True
+            self.client.set_bool(self.__gconfDir + "/loginPassed",
+                                 self.loginPassed)
+        else:
+            msg = "Login failed"
+
+        message = gtk.MessageDialog(None,
+            gtk.DIALOG_MODAL | gtk.DIALOG_DESTROY_WITH_PARENT,
+            gtk.MESSAGE_INFO, gtk.BUTTONS_OK, msg)
+        message.run()
+        message.destroy()
 
     def confluenceLogin(self):
         try:
-            confluenceApi.singleton(self.url).login(
-                self.username, self.password)
-            return True
+            self.confluence = Confluence(self.url, True)
+            if self.confluence.login(self.username, self.password) is True:
+                self.confluence.logout()
+                return True
         except Exception, err:
-            message = gtk.MessageDialog(None,
-                gtk.DIALOG_MODAL | gtk.DIALOG_DESTROY_WITH_PARENT,
-                gtk.MESSAGE_ERROR, gtk.BUTTONS_OK, err.message.__str__())
-            message.run()
-            message.destroy()
             return False
